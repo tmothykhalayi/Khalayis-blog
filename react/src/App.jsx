7b5da59960cd4ee7db75190db8851c06601c6754
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-07 17:54:26 +0300

    update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,10 @@
+import{UserProfile} from "./components/UserProfile";
 export default function App(){
 return (
     <div>
         <h1>Root component</h1>
+        <UserProfile/>
     </div>
-)
+);
 
 }
---
+++ react/src/components/UserProfile.jsx
@@ -0,0 +1,21 @@
+export function UserProfile(){
+    return (
+    <div>
+        <p> Username :Timothy</p>
+        <div>
+         <span>Email:</span>
+         <span>Timothykhalayi96@gmail.com</span>
+         </div>
+        <section>
+            <span>Favorite foods:</span>
+            <br />
+            <ul>
+                <li>Chapati</li>
+                <li>Homelet Eggs</li>
+                <li>Bread</li>
+            </ul>
+        </section>
+    
+    </div>
+    );
+}
