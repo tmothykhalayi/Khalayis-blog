e4eb5b69fd7c65d18f62962f33aa651f1595cd09
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-11 11:36:40 +0300

    update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,20 +1,29 @@
-export default  function App(){
-    const isauthenticated = false;
-    if (isauthenticated) {
-        return (
-            <div>
-                <h1>welcome ,user!</h1>
-            </div>
-        );
-        return(
-            <div>
-                <span>you are not logged in</span>
-            </div>
-            
-        );
-        <div>
-            <RegistrationForm/>
-            </div>
-        
+import React, { useState } from "react";
+import { UserDetails } from "./components/UserDetails";
+
+export default function App() {
+  // Initializing the state with multiple users
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "nandwa",
+      password: "123456",
+      email: "timothynandwa@gmail.com",
+    },
+    {
+      id: 2,
+      username: "Esthy Ambetsa",
+      password: "password123",
+      email: "esthy.ambetsa@gmail.com",
     }
+  ]);
+
+  return (
+    <div>
+      <h1>Welcome to the App</h1>
+      {users.map((user) => (
+        <UserDetails key={user.id} user={user} />
+      ))}
+    </div>
+  );
 }
